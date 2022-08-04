de4491268ecf02b8d33c23b32a172dff91c99edb
Omri Ruvio <omriruvio@gmail.com>
2022-08-04 23:34:42 +0300

    leet: valid palindrome

---
+++ valid-palindrome.test.js
@@ -0,0 +1,15 @@
+const isPalindrome = (str) => {
+  const normalized = str.replace(/(\W|_)/g, '').toLowerCase();
+  return normalized === normalized.split('').reverse().join('');
+};
+
+// isPalindrome('A man, a plan, a canal: Panama');
+
+describe('Checks if a valid palindrome', () => {
+  it('Returns true or false for valid/invalid palindromes', () => {
+    expect(isPalindrome('A man, a plan, a canal: Panama')).toBeTruthy();
+    expect(isPalindrome('race a car')).toBeFalsy();
+    expect(isPalindrome('race car')).toBeTruthy();
+    expect(isPalindrome('ab_a')).toBeTruthy();
+  });
+});
